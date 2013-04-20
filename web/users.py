0be7d573e190730ad5a453d0f11be239d7247f70
alexandru <msg4alex@gmail.com>
2013-04-20 14:31:11 +0300

    readme si showwoff

--- web/users.py
+++ web/users.py
@@ -1,6 +1,5 @@
 import hashlib
 
 def signup_user(db, username, password):
-	md5pass = hashlib.md5(password).hexdigest()
-	if db.find ({"email":username, "password", md5pass}).count() > 0:
+	
 		
--- web/web.py
+++ web/web.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
-frpm pymongo import MongoClient
+from pymongo import MongoClient
+import hashlib
 
 web = Flask (__name__)
 web.debug = True
@@ -11,7 +12,12 @@ def start():
 	return render_template ("start.html")
 
 @web.route ("/signup", mathods=['POST'])
-def signup
+def signup():
+
+@web.route ("/login", mathods=['POST'])
+def signup():
+md5pass = hashlib.md5(password).hexdigest()
+	if db.find ({"email":username, "password", md5pass}).count() > 0:
 
 if __name__ == '__main__':
 	web.run (host="0.0.0.0", port=8000)
