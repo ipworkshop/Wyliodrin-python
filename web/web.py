99c065a04f530d7f6030a58232735e997db0a397
alexandru <msg4alex@gmail.com>
2013-04-20 12:38:35 +0300

    flask

---
+++ web/web.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+web = Flask (__name__)
+
+@web.route ("/")
+def start():
+	return render_template ("start.html")
+
+
+if __name__ == '__main__':
+	app.run (host="0.0.0.0")
